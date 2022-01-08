faca01c5e39815e524e61abff463d9a5d828c1ac
SkolotajsRaivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-01-08 13:02:59 +0000

    Added comments and translated variable names to lv

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function guestCounter() {
   var cookie = document.cookie;
 
-  if (document.cookie == "") { // Ja cepums ir tukšs (apmeklē 1. reizi), mainīgajam iestata vērtību 1
-    var number = 1;
+  if (cookie == "") { // Ja cepums ir tukšs (apmeklē 1. reizi), mainīgajam iestata vērtību 1
+    var skaits = 1;
   }
-  
-  for (var i = 1; i < cookie.length; i++) { // Nolasa pašreizējo apmeklējumu skaitu
-    if (cookie.charAt(i) == "=") { // Sameklē vienādības zīmi
-      var number = cookie.substr(i+1); // Skaitli, kas seko aiz vienādības, ieglabā mainīgajā
+  else {
+    for (var i = 1; i < cookie.length; i++) { // Nolasa pašreizējo apmeklējumu skaitu
+      if (cookie.charAt(i) == "=") { // Sameklē vienādības zīmi
+        var skaits = cookie.substr(i+1); // Skaitli, kas seko aiz vienādības, ieglabā mainīgajā
+      }
     }
   }
-  alert("Šis ir "+number+". apmeklējums"); // Izvada paziņojumu
-  number++;
-  document.cookie = "apmeklējumi="+number;
+  alert("Šis ir "+skaits+". apmeklējums"); // Izvada paziņojumu
+  skaits++; // Palielina skaitu
+  document.cookie = "apmeklējumi="+skaits; // Ieraksta datus sīkdatnē
 }
